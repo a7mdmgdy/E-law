ef39fce95eac585f9bfde5b3273e5cc10aaeab44
a7mdmgdy <ahmed.magdy9.am@gmail.com>
2023-06-17 22:31:31 +0300

    init project

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function getUserData() {
+    let userEmail = document.getElementById("exampleInputEmail1").value
+    let userPass = document.getElementById("exampleInputPassword1").value
+    console.log("emoail" , userEmail) 
+
+    // if (userEmail ="" || userPass == "") {
+    //     console.log("please enter your data")
+    // }else if(userEmail && userPass){
+        localStorage.setItem("email", userEmail)
+        console.log("emoail" , userEmail) 
+        localStorage.setItem("password", userPass)
+        window.location = "categories.html"
+    // }
+}
+
+function isAuth() {
+    if (localStorage.getItem("password")) {
+        window.location = "calendar.html"
+        return true;
+    } else {
+        console.log("no auth");
+
+        window.location = "/Login.html"
+    }
+}
